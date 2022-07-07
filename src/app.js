16ce010dbff4d9237e2207a86040052278245c95
Flexxelexx <alexxkooij@hotmail.com>
2022-07-07 16:56:37 +0200

    lekker bezig

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+const API_KEY = 'https://restcountries.com/v2/all';
+
+async function fetchCountries () {
+
+const countryList = document.getElementById('countries-container')
+
+    try {
+        const response = await axios.get(API_KEY)
+        countryList.innerHTML += `
+            <li class="${response.data[0].region}">
+            ${response.data[0].name} has a population of ${response.data[0].population} people.</li>
+            <img src="${response.data[0].flags.png}">
+        `
+        console.log(response.data)
+    }
+
+    catch (e) {
+        console.log(e)
+    }
+}
+
+fetchCountries()
